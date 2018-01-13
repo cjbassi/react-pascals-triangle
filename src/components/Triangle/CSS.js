1572746a2ab32f2a6cc025abc9c73cecccf86649
Caleb Bassi <calebjbassi@gmail.com>
2018-01-12 22:12:14 -0800

    Refactored CSS into seperate files

---
+++ src/components/Triangle/CSS.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+const TriangleCell = styled.div`
+    min-width: 30px;
+    min-height: 20px;
+    border: 1px solid black;
+    display: inline-block;
+    margin: 1px;
+`
+
+const Triangle = styled.div`
+    text-align: center;
+`
+
+export {
+  TriangleCell,
+  Triangle,
+}
